3cd3d0fd72427012835fbf97e64e9395bd76b311
Hakim <ozil.hakim@gmail.com>
2020-12-23 16:42:49 -0500

    make the navbar alive

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import classes from './Navbar.module.css'
+import {NavLink} from 'react-router-dom'
 function Navbar() {
     return (
         <div className={classes.container}>
             <ul>
-                <li><a className={classes.navLink} href='/Me'>Me</a></li>
-                <li><a className={classes.navLink} href='Projects'>Projects</a></li>
+                <li><NavLink className={classes.navLink} to='/' exact activeClassName={classes.activeLink}>Me</NavLink></li>
+                <li><NavLink className={classes.navLink} to='/projects' activeClassName={classes.activeLink}>Projects</NavLink></li>
             </ul>
         </div>
     )

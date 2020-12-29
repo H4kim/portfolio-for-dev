364dffb723ce730189ba90d97ec068fab80ccfe6
Hakim <ozil.hakim@gmail.com>
2020-12-28 21:38:16 -0500

    change the baseUrl

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={"h4kim.me"}>
+    <BrowserRouter basename={"/portfolio"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
